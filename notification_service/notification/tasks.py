ba160d6a7bbfc7073c8f5eb35e03bf01280cdc93
Mostafa <mostafa.babaii@gmail.com>
2024-07-24 01:53:43 +0330

    Add max retry for otp celery task.

--- notification_service/notification/tasks.py
+++ notification_service/notification/tasks.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from notification_service.celery import app
 
 
-@app.task(name='send_otp', queue='otp')
+@app.task(name='send_otp', queue='otp', max_retries=3)
 def send_otp(user_id):
     # Our logic to send OTP via provider API
     pass
